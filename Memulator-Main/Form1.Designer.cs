b43bf6ab58ee70aad9e7140a31e5923f7ded0a93
FourthRuffle <fodor.gyozo.benedek@gmail.com>
2023-10-01 22:35:26 +0200

    Fixed button layout to match documentation

--- Memulator-Main/Form1.Designer.cs
+++ Memulator-Main/Form1.Designer.cs
@@ -31,8 +31,8 @@
             this.factorial = new System.Windows.Forms.Button();
             this.squareroot = new System.Windows.Forms.Button();
             this.multiply = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.clear = new System.Windows.Forms.Button();
+            this.displayEQ = new System.Windows.Forms.TextBox();
+            this.del = new System.Windows.Forms.Button();
             this.allclear = new System.Windows.Forms.Button();
             this.plus = new System.Windows.Forms.Button();
             this.minus = new System.Windows.Forms.Button();
@@ -50,191 +50,196 @@
             this.num8 = new System.Windows.Forms.Button();
             this.num7 = new System.Windows.Forms.Button();
             this.Mode = new System.Windows.Forms.Button();
+            this.logxy = new System.Windows.Forms.Button();
+            this.pow = new System.Windows.Forms.Button();
+            this.displayOP = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // factorial
             // 
-            this.factorial.Location = new System.Drawing.Point(347, 440);
+            this.factorial.Location = new System.Drawing.Point(197, 276);
             this.factorial.Name = "factorial";
-            this.factorial.Size = new System.Drawing.Size(99, 95);
+            this.factorial.Size = new System.Drawing.Size(50, 50);
             this.factorial.TabIndex = 53;
             this.factorial.Text = "!x";
             this.factorial.UseVisualStyleBackColor = true;
             // 
             // squareroot
             // 
-            this.squareroot.Location = new System.Drawing.Point(452, 439);
+            this.squareroot.Location = new System.Drawing.Point(253, 277);
             this.squareroot.Name = "squareroot";
-            this.squareroot.Size = new System.Drawing.Size(99, 95);
+            this.squareroot.Size = new System.Drawing.Size(50, 50);
             this.squareroot.TabIndex = 52;
             this.squareroot.Text = "sqrt";
             this.squareroot.UseVisualStyleBackColor = true;
             // 
             // multiply
             // 
-            this.multiply.Location = new System.Drawing.Point(347, 541);
+            this.multiply.Location = new System.Drawing.Point(197, 333);
             this.multiply.Name = "multiply";
-            this.multiply.Size = new System.Drawing.Size(99, 95);
+            this.multiply.Size = new System.Drawing.Size(50, 50);
             this.multiply.TabIndex = 51;
             this.multiply.Text = "*";
             this.multiply.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // displayEQ
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 18);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(560, 20);
-            this.textBox1.TabIndex = 50;
+            this.displayEQ.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.displayEQ.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.displayEQ.Location = new System.Drawing.Point(29, 12);
+            this.displayEQ.Name = "displayEQ";
+            this.displayEQ.Size = new System.Drawing.Size(274, 20);
+            this.displayEQ.TabIndex = 50;
             // 
-            // clear
+            // del
             // 
-            this.clear.Location = new System.Drawing.Point(452, 339);
-            this.clear.Name = "clear";
-            this.clear.Size = new System.Drawing.Size(99, 95);
-            this.clear.TabIndex = 49;
-            this.clear.Text = "C";
-            this.clear.UseVisualStyleBackColor = true;
+            this.del.Location = new System.Drawing.Point(253, 165);
+            this.del.Name = "del";
+            this.del.Size = new System.Drawing.Size(50, 50);
+            this.del.TabIndex = 49;
+            this.del.Text = "DEL";
+            this.del.UseVisualStyleBackColor = true;
             // 
             // allclear
             // 
-            this.allclear.Location = new System.Drawing.Point(347, 338);
+            this.allclear.Location = new System.Drawing.Point(197, 164);
             this.allclear.Name = "allclear";
-            this.allclear.Size = new System.Drawing.Size(99, 95);
+            this.allclear.Size = new System.Drawing.Size(50, 50);
             this.allclear.TabIndex = 48;
             this.allclear.Text = "AC";
             this.allclear.UseVisualStyleBackColor = true;
             // 
             // plus
             // 
-            this.plus.Location = new System.Drawing.Point(347, 642);
+            this.plus.Location = new System.Drawing.Point(197, 389);
             this.plus.Name = "plus";
-            this.plus.Size = new System.Drawing.Size(99, 95);
+            this.plus.Size = new System.Drawing.Size(50, 50);
             this.plus.TabIndex = 47;
             this.plus.Text = "+";
             this.plus.UseVisualStyleBackColor = true;
             // 
             // minus
             // 
-            this.minus.Location = new System.Drawing.Point(452, 643);
+            this.minus.Location = new System.Drawing.Point(253, 389);
             this.minus.Name = "minus";
-            this.minus.Size = new System.Drawing.Size(99, 94);
+            this.minus.Size = new System.Drawing.Size(50, 50);
             this.minus.TabIndex = 46;
             this.minus.Text = "-";
             this.minus.UseVisualStyleBackColor = true;
             // 
             // division
             // 
-            this.division.Location = new System.Drawing.Point(452, 540);
+            this.division.Location = new System.Drawing.Point(253, 333);
             this.division.Name = "division";
-            this.division.Size = new System.Drawing.Size(99, 95);
+            this.division.Size = new System.Drawing.Size(50, 50);
             this.division.TabIndex = 45;
             this.division.Text = "/";
             this.division.UseVisualStyleBackColor = true;
             // 
             // equals
             // 
-            this.equals.Location = new System.Drawing.Point(242, 642);
+            this.equals.Location = new System.Drawing.Point(141, 388);
             this.equals.Name = "equals";
-            this.equals.Size = new System.Drawing.Size(99, 95);
+            this.equals.Size = new System.Drawing.Size(50, 50);
             this.equals.TabIndex = 44;
             this.equals.Text = "=";
             this.equals.UseVisualStyleBackColor = true;
             // 
             // num0
             // 
-            this.num0.Location = new System.Drawing.Point(137, 643);
+            this.num0.Location = new System.Drawing.Point(85, 389);
             this.num0.Name = "num0";
-            this.num0.Size = new System.Drawing.Size(99, 95);
+            this.num0.Size = new System.Drawing.Size(50, 50);
             this.num0.TabIndex = 43;
             this.num0.Text = "0";
             this.num0.UseVisualStyleBackColor = true;
             // 
             // dot
             // 
-            this.dot.Location = new System.Drawing.Point(32, 642);
+            this.dot.Location = new System.Drawing.Point(29, 388);
             this.dot.Name = "dot";
-            this.dot.Size = new System.Drawing.Size(99, 95);
+            this.dot.Size = new System.Drawing.Size(50, 50);
             this.dot.TabIndex = 42;
             this.dot.Text = ".";
             this.dot.UseVisualStyleBackColor = true;
             // 
             // num3
             // 
-            this.num3.Location = new System.Drawing.Point(242, 540);
+            this.num3.Location = new System.Drawing.Point(141, 333);
             this.num3.Name = "num3";
-            this.num3.Size = new System.Drawing.Size(99, 95);
+            this.num3.Size = new System.Drawing.Size(50, 50);
             this.num3.TabIndex = 41;
             this.num3.Text = "3";
             this.num3.UseVisualStyleBackColor = true;
             // 
             // num2
             // 
-            this.num2.Location = new System.Drawing.Point(137, 540);
+            this.num2.Location = new System.Drawing.Point(85, 333);
             this.num2.Name = "num2";
-            this.num2.Size = new System.Drawing.Size(99, 95);
+            this.num2.Size = new System.Drawing.Size(50, 50);
             this.num2.TabIndex = 40;
             this.num2.Text = "2";
             this.num2.UseVisualStyleBackColor = true;
             // 
             // num1
             // 
-            this.num1.Location = new System.Drawing.Point(32, 541);
+            this.num1.Location = new System.Drawing.Point(29, 332);
             this.num1.Name = "num1";
-            this.num1.Size = new System.Drawing.Size(99, 95);
+            this.num1.Size = new System.Drawing.Size(50, 50);
             this.num1.TabIndex = 39;
             this.num1.Text = "1";
             this.num1.UseVisualStyleBackColor = true;
             // 
             // num6
             // 
-            this.num6.Location = new System.Drawing.Point(242, 440);
+            this.num6.Location = new System.Drawing.Point(141, 277);
             this.num6.Name = "num6";
-            this.num6.Size = new System.Drawing.Size(99, 95);
+            this.num6.Size = new System.Drawing.Size(50, 50);
             this.num6.TabIndex = 38;
             this.num6.Text = "6";
             this.num6.UseVisualStyleBackColor = true;
             // 
             // num5
             // 
-            this.num5.Location = new System.Drawing.Point(137, 439);
+            this.num5.Location = new System.Drawing.Point(85, 277);
             this.num5.Name = "num5";
-            this.num5.Size = new System.Drawing.Size(99, 95);
+            this.num5.Size = new System.Drawing.Size(50, 50);
             this.num5.TabIndex = 37;
             this.num5.Text = "5";
             this.num5.UseVisualStyleBackColor = true;
             // 
             // num4
             // 
-            this.num4.Location = new System.Drawing.Point(32, 440);
+            this.num4.Location = new System.Drawing.Point(29, 276);
             this.num4.Name = "num4";
-            this.num4.Size = new System.Drawing.Size(99, 95);
+            this.num4.Size = new System.Drawing.Size(50, 50);
             this.num4.TabIndex = 36;
             this.num4.Text = "4";
             this.num4.UseVisualStyleBackColor = true;
             // 
             // num9
             // 
-            this.num9.Location = new System.Drawing.Point(242, 339);
+            this.num9.Location = new System.Drawing.Point(141, 221);
             this.num9.Name = "num9";
-            this.num9.Size = new System.Drawing.Size(99, 95);
+            this.num9.Size = new System.Drawing.Size(50, 50);
             this.num9.TabIndex = 35;
             this.num9.Text = "9";
             this.num9.UseVisualStyleBackColor = true;
             // 
             // num8
             // 
-            this.num8.Location = new System.Drawing.Point(137, 338);
+            this.num8.Location = new System.Drawing.Point(85, 221);
             this.num8.Name = "num8";
-            this.num8.Size = new System.Drawing.Size(99, 95);
+            this.num8.Size = new System.Drawing.Size(50, 50);
             this.num8.TabIndex = 34;
             this.num8.Text = "8";
             this.num8.UseVisualStyleBackColor = true;
             // 
             // num7
             // 
-            this.num7.Location = new System.Drawing.Point(32, 338);
+            this.num7.Location = new System.Drawing.Point(29, 220);
             this.num7.Name = "num7";
-            this.num7.Size = new System.Drawing.Size(99, 95);
+            this.num7.Size = new System.Drawing.Size(50, 50);
             this.num7.TabIndex = 33;
             this.num7.Text = "7";
             this.num7.UseVisualStyleBackColor = true;
@@ -242,23 +247,55 @@
             // Mode
             // 
             this.Mode.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.Mode.Location = new System.Drawing.Point(32, 272);
+            this.Mode.Location = new System.Drawing.Point(29, 163);
             this.Mode.Name = "Mode";
             this.Mode.Size = new System.Drawing.Size(99, 51);
             this.Mode.TabIndex = 32;
             this.Mode.Text = "Mode";
             this.Mode.UseVisualStyleBackColor = false;
             // 
+            // logxy
+            // 
+            this.logxy.Location = new System.Drawing.Point(197, 220);
+            this.logxy.Name = "logxy";
+            this.logxy.Size = new System.Drawing.Size(50, 50);
+            this.logxy.TabIndex = 54;
+            this.logxy.Text = "logxY";
+            this.logxy.UseVisualStyleBackColor = true;
+            // 
+            // pow
+            // 
+            this.pow.Location = new System.Drawing.Point(253, 221);
+            this.pow.Name = "pow";
+            this.pow.Size = new System.Drawing.Size(50, 50);
+            this.pow.TabIndex = 55;
+            this.pow.Text = "Pow";
+            this.pow.UseVisualStyleBackColor = true;
+            // 
+            // displayOP
+            // 
+            this.displayOP.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.displayOP.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.displayOP.Location = new System.Drawing.Point(29, 137);
+            this.displayOP.Name = "displayOP";
+            this.displayOP.ReadOnly = true;
+            this.displayOP.Size = new System.Drawing.Size(274, 20);
+            this.displayOP.TabIndex = 56;
+            this.displayOP.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
             // Memulator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(584, 761);
+            this.ClientSize = new System.Drawing.Size(334, 461);
+            this.Controls.Add(this.displayOP);
+            this.Controls.Add(this.pow);
+            this.Controls.Add(this.logxy);
             this.Controls.Add(this.factorial);
             this.Controls.Add(this.squareroot);
             this.Controls.Add(this.multiply);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.clear);
+            this.Controls.Add(this.displayEQ);
+            this.Controls.Add(this.del);
             this.Controls.Add(this.allclear);
             this.Controls.Add(this.plus);
             this.Controls.Add(this.minus);
@@ -276,7 +313,9 @@
             this.Controls.Add(this.num8);
             this.Controls.Add(this.num7);
             this.Controls.Add(this.Mode);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Memulator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Memulator";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -288,8 +327,8 @@
         private System.Windows.Forms.Button factorial;
         private System.Windows.Forms.Button squareroot;
         private System.Windows.Forms.Button multiply;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.TextBox displayEQ;
+        private System.Windows.Forms.Button del;
         private System.Windows.Forms.Button allclear;
         private System.Windows.Forms.Button plus;
         private System.Windows.Forms.Button minus;
@@ -307,6 +346,9 @@
         private System.Windows.Forms.Button num8;
         private System.Windows.Forms.Button num7;
         private System.Windows.Forms.Button Mode;
+        private System.Windows.Forms.Button logxy;
+        private System.Windows.Forms.Button pow;
+        private System.Windows.Forms.TextBox displayOP;
     }
 }
 
